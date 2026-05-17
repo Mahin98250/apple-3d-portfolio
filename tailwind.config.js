8a1c4e3d2192fa92a8cefcc55e32f12ca72d14a3
Mahin98250 <patelmahin140@gmail.com>
2026-05-17 11:30:11 +0530

    Add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#050816',
+        accent: '#7dd3fc'
+      }
+    }
+  },
+  plugins: []
+}
